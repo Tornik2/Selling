a4a7e7cd3049169ed6cd2670ed5902ee0435158e
SabaShagidzeMain <sabadoeswebdev@gmail.com>
2024-11-19 18:13:47 +0400

    Refactor(utils): utility files to tsx
    . "QuantitySelector": Added typescript typings for "stock" and "minOrder" props;
    . "Logo": Defined prop types for "id", "className" and "transform" attributes;
    . "FeatureKey" prop: Removed unsupported "featurekey" attribute;
    . Added missing TypeScript typings and refactored relevant code;

---
+++ src/app/[lang]/utils/hamburger.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Hamburger(): JSX.Element {
+  return (
+    <div className="hamburger">
+      <div className="stick"></div>
+      <div className="stick"></div>
+      <div className="stick"></div>
+    </div>
+  );
+}
