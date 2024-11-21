7e410a72c7125760cb94efbe74937dd49d17652c
sangambash7 <san.gambashidze@gmail.com>
2024-11-21 23:22:13 +0200

    Added dictionary words for products

--- src/app/[lang]/products/[id]/page.tsx
+++ src/app/[lang]/products/[id]/page.tsx
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar, faStarHalfAlt } from '@fortawesome/free-solid-svg-icons';
 import QuantitySelector from '../../utils/quantitySelector';
 import { getItemById } from '../../utils/supabaseUtils'; // Import the function to fetch data from Supabase
-import { Database } from '../../utils/database.types'; // Make sure you have this type for your database
+import { getDictionary } from '../../../../../get-dictionaries';
 
 // Function to render the stars based on rating
 const renderStars = (rating: number) => {
@@ -44,10 +44,11 @@ interface ProductPageProps {
 
 export default async function ProductPage({ params }: ProductPageProps) {
   const { id } = params; // Get the ID from URL parameters
+  const dictionary = await getDictionary(params.lang as 'en');
 
   // Fetch product details from Supabase
   const product = await getItemById(
-    `products_${params.lang}` as 'Services' | 'posts' | 'products',
+    `products_${params.lang}` as 'products',
     id
   );
 
@@ -95,10 +96,10 @@ export default async function ProductPage({ params }: ProductPageProps) {
           </div>
           <div className="stocks">
             <p className="stock">
-              Amount in stock: <span>{product.stock}</span>
+              {dictionary.productsID.stock}: <span>{product.stock}</span>
             </p>
             <p className="minOrder">
-              Minimum order quantity:{' '}
+              {dictionary.productsID.order}:{' '}
               <span>{product.minimumOrderQuantity}</span>
             </p>
             <QuantitySelector
@@ -107,18 +108,24 @@ export default async function ProductPage({ params }: ProductPageProps) {
             />
           </div>
           <div className="buttons">
-            <button className="addCartBtn">Add to cart</button>
-            <button className="buyBtn">Buy now</button>
+            <button className="addCartBtn">{dictionary.productsID.cart}</button>
+            <button className="buyBtn">{dictionary.productsID.buy}</button>
           </div>
         </div>
         <div className="textSection">
           <p className="desc">{product.description}</p>
           <div className="additionalInfo">
             <div className="dimensions">
-              <h6>Dimensions:</h6>
-              <p>Width: {product.width}</p>
-              <p>Height: {product.height}</p>
-              <p>Depth: {product.depth}</p>
+              <h6>{dictionary.productsID.dimensions}:</h6>
+              <p>
+                {dictionary.productsID.width}: {product.width}
+              </p>
+              <p>
+                {dictionary.productsID.height}: {product.height}
+              </p>
+              <p>
+                {dictionary.productsID.depth}: {product.depth}
+              </p>
             </div>
           </div>
         </div>
