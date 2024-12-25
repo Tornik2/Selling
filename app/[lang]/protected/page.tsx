b66a80bc3e9a7afa3edcfda63539ac7903d13e79
otogelashvili <otarbasilevsi@gmail.com>
2024-12-25 18:05:07 +0400

    refactor: remove extra /protected route handling

---
+++ app/[lang]/protected/page.tsx
@@ -0,0 +1,7 @@
+export default function loggedInPage() {
+  return (
+    <main className="flex flex-1 items-center justify-center">
+      <h1>You are aleady logged in</h1>
+    </main>
+  );
+}
--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,8 +1,5 @@
 import { createBrowserClient } from '@supabase/ssr';
 
-// createBrowserClient: This function is from the @supabase/ssr package
-// and is specifically designed for server-side rendering (SSR) environments like Next.js.
-//  It sets up a Supabase client for interacting with your Supabase backend.
 export const createClient = () =>
   createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
